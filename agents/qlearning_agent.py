bdaa92d2b8a07774a088b34b6810630d0e3724c7
Eetu Rantala <rantala.eetu@gmail.com>
2020-04-14 19:32:05 +0300

    Lisätty jonkin sortin q-learning agentti

--- agents/qlearning_agent.py
+++ agents/qlearning_agent.py
@@ -1,6 +1,40 @@
 from .agent import Agent
+import numpy as np
+np.set_printoptions(threshold=np.inf)
 
 class QLearningAgent(Agent):
-    
+    # stepsize
+    alpha = 0.1
+
+    epsilon = 0.3
+
+    discount = 0.9
+
+    def __init__(self, action_space, observation_space):
+        super().__init__(action_space)
+        self.q = np.zeros((observation_space.n, action_space.n))
+        self.s = None
+        self.a = None
+        self.r = None
+
     def get_action(self, observation, reward, done):
-        pass
+        #print(observation,reward,done)
+
+        if self.s is not None:
+            self.q[self.s][self.a] = self.q[self.s][self.a] + self.alpha*(self.r + self.discount * (np.max(
+                self.q[observation])) - self.q[self.s][self.a])
+            #print(self.q[observation][self.a])
+
+        self.s, self.r = observation, reward
+
+        # Epsilon-greedy action selection
+        if np.random.random_sample() <= self.epsilon:
+            self.a = self.action_space.sample()
+        else:
+            self.a = np.argmax(self.q[observation])
+
+        return self.a
+
+    def printq(self):
+        print(self.q)
+
--- main.py
+++ main.py
@@ -1,27 +1,47 @@
 import gym
 from agents.random_agent import RandomAgent
+from agents.qlearning_agent import QLearningAgent
 
 env = gym.make('Taxi-v3')
 
-observation = env.reset()
-reward = 0
-done = False
-penalties = 0
-episodes = 10000
+
+total_penalties = 0
+episodes = 2000
+completions = 0
 
 agent = RandomAgent(env.action_space)
-env.render()
+agent2 = QLearningAgent(env.action_space, env.observation_space)
 
-for i in range(episodes):
-    
-    action = agent.get_action(observation, reward, done)
-    observation, reward, done, info = env.step(action)
+for episode in range(episodes):
+    observation = env.reset()
+    reward = 0
+    done = False
+    penalties = 0
+    #env.render()
+    for i in range(200):
+        action = agent2.get_action(observation, reward, done)
+        observation, reward, done, info = env.step(action)
 
-    if reward == -10:
-        penalties += 1
 
-    if done:
-        env.render()
-        print("Episode finished after", i, "random actions")
-        print("Penalties", penalties)
-        break
+        if reward == -10:
+            penalties += 1
+
+        if done:
+            # env.render()
+            # print("Episode finished after", i, "time steps")
+            # print("Penalties occured", penalties)
+            #
+            if reward == 20:
+                #print("Objective completed")
+                completions += 1
+            # else:
+            #     print("Objective not completed")
+
+            break
+    total_penalties += penalties
+
+print()
+print("Average penalties over episode", total_penalties / episodes)
+print("Completions", completions)
+
+#agent2.printq()
