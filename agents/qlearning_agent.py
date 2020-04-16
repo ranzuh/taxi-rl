61f78dbdcd6b5cfe3a8dd2607639f699ae90bd09
Eetu Rantala <rantala.eetu@gmail.com>
2020-04-16 11:12:24 +0300

    Ei toimi kunnolla :/

--- agents/qlearning_agent.py
+++ agents/qlearning_agent.py
@@ -1,40 +1,64 @@
 from .agent import Agent
 import numpy as np
+
 np.set_printoptions(threshold=np.inf)
 
+
 class QLearningAgent(Agent):
-    # stepsize
+    # learning rate
     alpha = 0.1
 
-    epsilon = 0.3
+    # how often random move
+    epsilon = 0.1
 
-    discount = 0.9
+    # discount future rewards
+    discount = 0.6
 
     def __init__(self, action_space, observation_space):
         super().__init__(action_space)
-        self.q = np.zeros((observation_space.n, action_space.n))
-        self.s = None
-        self.a = None
-        self.r = None
 
-    def get_action(self, observation, reward, done):
-        #print(observation,reward,done)
+        # initialize empty Q-table
+        self.Q = np.zeros((observation_space.n, action_space.n))
 
-        if self.s is not None:
-            self.q[self.s][self.a] = self.q[self.s][self.a] + self.alpha*(self.r + self.discount * (np.max(
-                self.q[observation])) - self.q[self.s][self.a])
-            #print(self.q[observation][self.a])
+        # initialize state, action and reward
+        self.s = self.a = self.r = None
 
-        self.s, self.r = observation, reward
+    def get_action(self, next_state, reward, done):
+        # print(observation,reward,done)
 
-        # Epsilon-greedy action selection
-        if np.random.random_sample() <= self.epsilon:
-            self.a = self.action_space.sample()
+        Q, s, a, r = self.Q, self.s, self.a, self.r
+        alpha, epsilon, discount = self.alpha, self.epsilon, self.discount
+
+        if done:
+            Q[s, None] = reward
+
+        if s is not None:
+            Q[s, a] = Q[s, a] + alpha * (r + discount * (np.max(Q[next_state])) - Q[s, a])
+            # print(self.q[observation][self.a])
+
+        if done:
+            self.s = self.a = self.r = None
         else:
-            self.a = np.argmax(self.q[observation])
+            self.s, self.r = next_state, reward
+
+            # Epsilon-greedy action selection
+            if np.random.random_sample() < epsilon:
+                self.a = self.action_space.sample()
+            else:
+                self.a = np.argmax(Q[next_state])
 
         return self.a
 
-    def printq(self):
-        print(self.q)
+    def get_learned_action(self, observation):
+        # print(self.q[observation])
+        # print(np.argmax(self.q[observation]))
+        return np.argmax(self.Q[observation])
 
+    def printq(self):
+        print(self.Q)
+
+    def save_table(self):
+        np.savetxt("table.csv", self.Q, delimiter=",")
+
+    def load_table(self):
+        self.Q = np.loadtxt(open("table.csv"), delimiter=",")
--- main.py
+++ main.py
@@ -4,22 +4,63 @@ from agents.qlearning_agent import QLearningAgent
 
 env = gym.make('Taxi-v3')
 
-
 total_penalties = 0
-episodes = 2000
+episodes = 2001
 completions = 0
 
 agent = RandomAgent(env.action_space)
 agent2 = QLearningAgent(env.action_space, env.observation_space)
 
 for episode in range(episodes):
+    if episode % 1000 == 0:
+        print(episode)
     observation = env.reset()
     reward = 0
     done = False
     penalties = 0
-    #env.render()
+    # env.render()
     for i in range(200):
+        # env.render()
         action = agent2.get_action(observation, reward, done)
+        observation, reward, done, info = env.step(action)
+
+        if reward == -10:
+            penalties += 1
+
+        if done:
+            agent2.get_action(observation, reward, done)
+            # env.render()
+            # print("Episode finished after", i, "time steps")
+            # print("Penalties occured", penalties)
+            #
+            if reward == 20:
+                # print("Objective completed")
+                completions += 1
+            # else:
+            #     print("Objective not completed")
+
+            break
+    total_penalties += penalties
+
+agent2.save_table()
+
+print()
+print("Average penalties over episode", total_penalties / episodes)
+print("Completions", completions)
+
+total_penalties = 0
+episodes = 200
+completions = 0
+
+for episode in range(episodes):
+    observation = env.reset()
+    reward = 0
+    done = False
+    penalties = 0
+    # env.render()
+    for i in range(200):
+        # env.render()
+        action = agent2.get_learned_action(observation)
         observation, reward, done, info = env.step(action)
 
 
@@ -27,12 +68,14 @@ for episode in range(episodes):
             penalties += 1
 
         if done:
+            # print(reward)
+            # print(reward)
             # env.render()
             # print("Episode finished after", i, "time steps")
             # print("Penalties occured", penalties)
             #
             if reward == 20:
-                #print("Objective completed")
+                # print("Objective completed")
                 completions += 1
             # else:
             #     print("Objective not completed")
