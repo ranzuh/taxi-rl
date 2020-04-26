5596479b3605a7c9effcee1961cebcfaecb7c9a6
Eetu Rantala <rantala.eetu@gmail.com>
2020-04-26 22:30:55 +0300

    Kommentointia lisätty

--- agents/agent.py
+++ agents/agent.py
@@ -4,7 +4,17 @@ class Agent:
         self.action_space = action_space
 
     def get_action(self, state):
+        """
+        Returns some action for given state
+        :param state: current state
+        :return action
+        """
         pass
 
     def get_policy(self, state):
+        """
+        Returns action according to policy
+        :param state: current state
+        :return action
+        """
         pass
--- agents/random_agent.py
+++ agents/random_agent.py
@@ -4,6 +4,7 @@ from .agent import Agent
 class RandomAgent(Agent):
 
     def get_action(self, state):
+        # Choose random action
         return self.action_space.sample()
 
     def get_policy(self, state):
